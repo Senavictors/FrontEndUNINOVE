bb2e79869ed4bb97b91f555f60d15eecc05ac685
Victor Sena <victorsena760@gmail.com>
2022-03-22 21:42:20 -0300

    Update script3_07-1.js

--- script3_07-1.js
+++ script3_07-1.js
@@ -1,10 +1,12 @@
-var nome = prompt ("Digite o nome");//"15"
-var nota = prompt ("Digite a nota");//"4"
-var result = num1 + num2;//result = "15" + "4" //result = "154"
-document.write ("result = "+result);//154
+var num1 = prompt ("Digite um número");//7.8 ///7
+num1 = parseFloat(num1);//15 número inteiro
+var num2 = prompt ("Digite um número");//2.2   ///2
+num2 = parseFloat(num2);//4 número inteiro
+
+var result = num1 + num2;//result = //result = 9
+document.write ("result = " + result);//
 /*
 Transformar
 parseInt - número inteiro
 parseFloat - número fracionado (float)
-
 */
