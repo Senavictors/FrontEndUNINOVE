d64b720f93700d235b1e3dbe72fa35f8b1aaf6ac
Victor Sena <victorsena760@gmail.com>
2022-03-22 21:26:14 -0300

    Add files via upload

---
+++ script3_07-1.js
@@ -0,0 +1,10 @@
+var nome = prompt ("Digite o nome");//"15"
+var nota = prompt ("Digite a nota");//"4"
+var result = num1 + num2;//result = "15" + "4" //result = "154"
+document.write ("result = "+result);//154
+/*
+Transformar
+parseInt - número inteiro
+parseFloat - número fracionado (float)
+
+*/
